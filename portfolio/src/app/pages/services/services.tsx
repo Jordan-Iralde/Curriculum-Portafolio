a084066bf4619f436fe49df0e56cf8d68576b0fa
Jordan Iralde <iraldejordan10@gmail.com>
2025-05-19 20:57:30 -0300

    services falta

--- portfolio/src/app/pages/services/services.tsx
+++ portfolio/src/app/pages/services/services.tsx
@@ -2,6 +2,8 @@ import styles from './services.module.css';
 
 export default function services() {
   return (
-    <p className={styles.servicios}>services</p>
+    <p className={styles.servicios}>
+    Contactate y pedi tu presupuesto sin compromiso.
+    </p>
   );
 }
